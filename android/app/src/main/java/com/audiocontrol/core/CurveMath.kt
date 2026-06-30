20514bc4522b371506f27c93c064f41bf53619d0
BPueschel <brianpueschel93@gmail.com>
2026-06-29 22:53:52 -0400

    feat(android): generalized passband curve math (TDD)

---
+++ android/app/src/main/java/com/audiocontrol/core/CurveMath.kt
@@ -0,0 +1,27 @@
+package com.audiocontrol.core
+
+import kotlin.math.ln
+import kotlin.math.log10
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.pow
+
+data class FilterCurveSpec(val freq: Int, val bypass: Boolean, val type: FilterType)
+
+fun curveDb(hpf: FilterCurveSpec, lpf: FilterCurveSpec, f: Double): Double {
+    var d = 0.0
+    if (!hpf.bypass) d += -10.0 * log10(1.0 + (hpf.freq / f).pow(hpf.type.exponent))
+    if (!lpf.bypass) d += -10.0 * log10(1.0 + (f / lpf.freq).pow(lpf.type.exponent))
+    return d
+}
+
+fun curveXNorm(f: Double): Double = ln(f / 20.0) / ln(32.0)
+
+fun curveYNorm(db: Double): Double = min(30.0, max(0.0, -db)) / 30.0
+
+fun curvePoints(hpf: FilterCurveSpec, lpf: FilterCurveSpec, n: Int = 160): List<Pair<Double, Double>> =
+    (0..n).map { i ->
+        val frac = i.toDouble() / n
+        val f = 20.0 * 32.0.pow(frac)
+        frac to curveYNorm(curveDb(hpf, lpf, f))
+    }
